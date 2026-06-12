dadcbf0e6eb393b648546529f1637301d79645c8
fredysiswanto <idloginfredy@gmail.com>
2026-06-12 11:24:39 +0700

    test

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: "foodtrip-api-ts",
-			cwd: "/home/ubuntu/foodtrip-api-ts",
+			cwd: "/home/ubuntu/foodtrip-api-ts/current",
 			script: "./dist/index.js",
 			instances: "1",
 			exec_mode: "fork",
